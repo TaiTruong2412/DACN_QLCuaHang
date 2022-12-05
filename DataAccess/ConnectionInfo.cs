a2b1341f16146c2d0f37af2fa378a75e424feb71
Tài Trương Minh <truongminhtai24122001@gmail.com>
2022-12-05 12:53:07 +0700

    Add project files.

---
+++ DataAccess/ConnectionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    internal class ConnectionInfo
+    {
+        public static string CONNECTION_STRING = @"Data Source=DESKTOP-EJF9DP9\SQLEXPRESS01;Initial Catalog=ShopManager;Integrated Security=True";
+    }
+}
